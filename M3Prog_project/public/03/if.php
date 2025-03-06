d860c2ac6ac8af18a350d9095c646c69f3ed2ac6
Tyrone <112290278+TyroneD08@users.noreply.github.com>
2025-03-06 09:48:03 +0100

    update1 6mrt

---
+++ M3Prog_project/public/03/if.php
@@ -0,0 +1,20 @@
+<?php
+
+$aantalOpVoorraad = 20;
+$opvoorraad = $aantalOpVoorraad > 0;
+
+//if($opvoorraad == true)
+if($opvoorraad == false)
+{
+ echo "niet op voorraad";
+}
+else
+{
+    echo "Artikel is op voorraad";
+}
+
+
+
+
+
+?>
