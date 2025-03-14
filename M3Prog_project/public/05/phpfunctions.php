08e13b7a60e525e91f843822df3174bc7b6c935f
Tyrone <112290278+TyroneD08@users.noreply.github.com>
2025-03-14 10:53:34 +0100

    05 docker

---
+++ M3Prog_project/public/05/phpfunctions.php
@@ -0,0 +1,16 @@
+<?php
+
+
+    $hoeLangIsDitWoord = "scrabbleWoord";
+    $woordLengte = strlen($hoeLangIsDitWoord);
+    print($woordLengte);
+
+
+    //vraag 1 antwoord = string
+    // vraag 2 antwoord = strlen returned een int (een gehele getal)
+    // vraag 3 antwoord = de lengte van de string
+
+
+    $metEenAnderLangWoord = "puzzelwoord";
+    $woordLengte = strlen($metEenAnderLangWoord);
+    print($woordLengte);
