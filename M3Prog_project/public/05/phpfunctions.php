adaf932a6a7baeac3396ac376e519dbf4a1e35a9
Tyrone <112290278+TyroneD08@users.noreply.github.com>
2025-03-16 15:24:00 +0100

    update zondag!

--- M3Prog_project/public/05/phpfunctions.php
+++ M3Prog_project/public/05/phpfunctions.php
@@ -14,3 +14,30 @@
     $metEenAnderLangWoord = "puzzelwoord";
     $woordLengte = strlen($metEenAnderLangWoord);
     print($woordLengte);
+
+    // vraag 4 antwoord = 2keer 
+
+
+    echo pow(2,2);
+    print("<br>");
+
+    $tweeMacht2 = pow(2,10);
+    echo $tweeMacht2;
+
+
+    $derdeMacht = pow(3,4);
+    print("<br>");
+    print(pow(3,4));
+
+    
+    //vraag 1 antwoord = dat doen we door print of echo te gebruiken
+
+
+    $naam = "Wario";
+    $naamGroot = strtoupper($naam);
+    print("<br>");
+    print($naamGroot);
+    
+    $naam = "Wario";
+    print("<br>");
+    print($naam);
